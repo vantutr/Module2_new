e3766096fd4537dd89c757c939497fc1b58c690b
vantutr <tranvantu06042001@gmail.com>
2025-04-17 16:01:37 +0700

    update buoi8

--- buoi8/baitap1/src/TennisGame.java
+++ buoi8/baitap1/src/TennisGame.java
@@ -1,61 +1,45 @@
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        if (player1Score == player2Score) {
+            return getEqualScore(player1Score);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            return getAdvancedOrWinScore(player1Score, player2Score);
+        } else {
+            return getNormalScore(player1Score, player2Score);
+        }
+    }
 
-            }
+    private static String getEqualScore(int score) {
+        switch (score) {
+            case 0: return "Love-All";
+            case 1: return "Fifteen-All";
+            case 2: return "Thirty-All";
+            case 3: return "Forty-All";
+            default: return "Deuce";
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private static String getAdvancedOrWinScore(int player1Score, int player2Score) {
+        int scoreDifference = player1Score - player2Score;
+
+        if (scoreDifference == 1) return "Advantage player1";
+        else if (scoreDifference == -1) return "Advantage player2";
+        else if (scoreDifference >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private static String getNormalScore(int player1Score, int player2Score) {
+        return getScoreName(player1Score) + "-" + getScoreName(player2Score);
+    }
+
+    private static String getScoreName(int score) {
+        switch (score) {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            case 3: return "Forty";
+            default: return "Unknown";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
